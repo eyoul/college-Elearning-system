c77f81ddffd225992774459c3dcd1a34e6e73b23
eyoul <49184150+eyoul@users.noreply.github.com>
2024-01-30 22:58:23 +0300

    Update report.py

--- elearning/report.py
+++ elearning/report.py
@@ -1,6 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for, session
 )
+
 from werkzeug.exceptions import abort
 
 from elearning .auth import login_required
